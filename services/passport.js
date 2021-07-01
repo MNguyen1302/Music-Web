f64ed6471c8e71539186337cadd6a9ee39d5679a
Mỹ Nguyên <77737848+Bee1001@users.noreply.github.com>
2021-07-01 10:45:58 +0700

    Update passport.js

--- services/passport.js
+++ services/passport.js
@@ -16,7 +16,7 @@ passport.deserializeUser((googleId, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://music-app-web.herokuapp.com/auth/google/callback'
+    callbackURL: '/auth/google/callback'
     },  (accessToken, refreshToken, object0, profile, done) => {
         if(profile.id) {
             User.findOne({googleId: profile.id})
